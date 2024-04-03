f1eacbf993b2c7628376c6cd31119dd3d6ae5242
MrNazeer <nazeerahamed1724@gmail.com>
2024-04-03 15:03:38 +0530

    secction 1 updated

--- src/components/Section1/Section1.jsx
+++ src/components/Section1/Section1.jsx
@@ -5,13 +5,13 @@ import "./style.scss";
 export default function Section1() {
   return (
     <section className="sec1">
-      <div className="sec1-text-wrap">
-        <h2 className="">YOU DON'T NEED A SILVER FORK TO EAT GOOD FOOD.</h2>
-        <p className="btn1">See Our Menu</p>
-        <p className="btn1">Special Offers</p>
-      </div>
-      <div className="sec1-img">
-        <img src={hero} alt="bg-img" />
+      <div className="sec1-cont">
+        <div className="sec1-text-wrap">
+          <p className="title">YOU DON'T NEED A SILVER FORK TO EAT GOOD FOOD.</p>
+          <p className="btn1">See Our Menu</p>
+          <p className="btn1">Special Offers</p>
+        </div>
+        <div className="sec1-img">{/* <img src={hero} alt="bg-img" /> */}</div>
       </div>
     </section>
   );
